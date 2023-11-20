87da0ea48c9a2e5c6c09759bc0656c1ce2e58d37
brom5033 <sjajdcjddlsl@naver.com>
2023-11-20 21:41:00 +0900

    feat(main-page): logout buuton add

--- src/components/templates/MainPage.tsx
+++ src/components/templates/MainPage.tsx
@@ -1,23 +1,34 @@
 import type { FC } from 'react';
 import { SwiperSlide } from 'swiper/react';
 // component
+import { Button } from '@components/atoms';
 import { Slider } from '@components/molecules/Slider';
+import { userModel } from '@stores/index';
 
 export const MainPage: FC = () => {
+    const userModelStore = userModel();
+
+    const handleLogoutClick = () =>{
+        userModelStore.emptyUser();
+    } 
+
     return (
-        <Slider>
-            <SwiperSlide>
-                <img src="/images/slider1.jpg" alt="" height="400px" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="/images/slider2.jpg" alt="" height="400px" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="/images/slider3.jpg" alt="" height="400px" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="/images/slider4.jpg" alt="" height="400px" />
-            </SwiperSlide>
-        </Slider>
+        <>
+            <Button onClick={handleLogoutClick}>로그아웃 테스트</Button>
+            <Slider>
+                <SwiperSlide>
+                    <img src="/images/slider1.jpg" alt="" height="400px" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/images/slider2.jpg" alt="" height="400px" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/images/slider3.jpg" alt="" height="400px" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="/images/slider4.jpg" alt="" height="400px" />
+                </SwiperSlide>
+            </Slider>
+        </>
     );
 };
