867fd6bbad875f39a75e01c614e137540d0bd42f
brom5033 <sjajdcjddlsl@naver.com>
2023-11-21 04:52:06 +0900

    style(card): height width 균일하게 적용

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -9,9 +9,9 @@ interface Props extends PropsWithChildren {
 
 export const Card: FC<Props> = ({ onClick, children, src, alt }) => {
     return (
-        <MUICard sx={{ maxWidth: '250px' }} onClick={onClick}>
-            <CardMedia component="img" src={src} alt={alt} />
-            <CardContent >{children}</CardContent>
+        <MUICard sx={{ maxWidth: '250px', margin: '24px', height: '520px' }} onClick={onClick}>
+            <CardMedia component="img" src={src} alt={alt} sx={{ height: '200px' }} />
+            <CardContent>{children}</CardContent>
         </MUICard>
     );
 };
